950220d07dc126397708795b2e89d598f9584367
tao.wang <tao.wang@smartriver.com.cn>
2019-02-28 18:25:35 +0800

    init maven repo update

--- util/src/main/java/com/chengchengtech/util/verify/RandomUtil.java
+++ util/src/main/java/com/chengchengtech/util/verify/RandomUtil.java
@@ -20,23 +20,15 @@ public class RandomUtil {
 
     private static final String VERIFY_CODE_BASE="0123456789";
 
-    public static String getRandomNumCode(int number) {
+    public static String getRandomNumCode(int count) {
         String codeNum = "";
         int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         Random random = new Random();
-        for (int i = 0; i < number; i++) {
+        for (int i = 0; i < count; i++) {
             //目的是产生足够随机的数，避免产生的数字重复率高的问题
             int next = random.nextInt(10000);
             codeNum += numbers[next % 10];
         }
         return codeNum;
-    }
-
-    public static void main(String[] args) {
-        HashSet<String> set = new HashSet<>();
-        for (int i = 0; i < 1000; i++) {
-            set.add( RandomStringUtils.random(4, "0123456789"));
-        }
-        System.out.println(set.size());
     }
 }
